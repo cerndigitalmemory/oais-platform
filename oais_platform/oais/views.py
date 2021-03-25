a34c607fe9324a22641e0bd2b4c4160910612d1f
Antonio Vivace <avivace4@gmail.com>
2021-03-25 08:53:44 +0100

    Add `harvest` and `task_status` basic views

--- oais_platform/oais/views.py
+++ oais_platform/oais/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
 from django.contrib.auth.models import User, Group
 from oais_platform.oais.models import Record
 from rest_framework import viewsets
 from rest_framework import permissions
-from oais_platform.oais.serializers import UserSerializer, GroupSerializer, RecordSerializer
+from oais_platform.oais.serializers import (
+    UserSerializer,
+    GroupSerializer,
+    RecordSerializer,
+)
+
+from .tasks import process
 
 
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -20,14 +28,27 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 class RecordViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows records to be viewed or edited.
     """
+
     queryset = Record.objects.all()
     serializer_class = RecordSerializer
     permission_classes = [permissions.IsAuthenticated]
+
+
+def harvest(request, rec_id):
+    task_id = process.apply_async()
+    return HttpResponse(f"You requested recid {rec_id}. Celery task is {task_id}")
+
+
+def task_status(request, task_id):
+    task = process.AsyncResult(task_id=task_id)
+    return HttpResponse(f"{task.status}, {task.info.get('bagit_res')}")
