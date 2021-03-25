21ccddf336e8198a4f84cb6727f1106dbe267f7a
Antonio Vivace <avivace4@gmail.com>
2021-03-25 08:52:33 +0100

    Add basic task to trigger harvesting with BagIt Create

--- oais_platform/oais/tasks.py
+++ oais_platform/oais/tasks.py
@@ -1,5 +1,18 @@
 from celery import shared_task
+from celery.decorators import task
+from bagit_create import main as bic
+from time import sleep
 
-@shared_task
-def add(x, y):
-    return x + y
+from celery.utils.log import get_task_logger
+
+logger = get_task_logger(__name__)
+
+
+@task(bind=True, name="process")
+def process(self):
+    logger.info("Task started")
+    res = bic.process(
+        recid=1,
+        source="cds",
+    )
+    self.update_state(state="PROGRESS", meta={"bagit_res": res})
