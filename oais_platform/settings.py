1e22c251e905dba203184c18a21a2f328a46c699
Marco Donadoni <marco.donadoni@cern.ch>
2021-06-17 17:11:38 +0200

    Add initial migration for Record

--- oais_platform/settings.py
+++ oais_platform/settings.py
@@ -134,3 +134,5 @@ REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
 }
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
