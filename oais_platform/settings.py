678085642cc83f3b5e91695c0c9bbc8e8c12ae5e
Marco Donadoni <marco.donadoni@cern.ch>
2021-06-30 14:53:34 +0200

    Enable session authentication
    Session authentication is needed by the browsable API

--- oais_platform/settings.py
+++ oais_platform/settings.py
@@ -137,6 +137,7 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 10,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
