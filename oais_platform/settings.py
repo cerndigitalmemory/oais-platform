9925ab6c6aa0b9b074fb97f0477b527921543895
Antonio Vivace <avivace4@gmail.com>
2021-09-16 10:43:11 +0200

    Set static folder as relative

--- oais_platform/settings.py
+++ oais_platform/settings.py
@@ -151,7 +151,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = "/static/"
+STATIC_URL = "static/"
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
