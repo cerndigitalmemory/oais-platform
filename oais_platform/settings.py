fd6bade2a7d5c9b6c9f6550e99b7817397f957eb
Antonio Vivace <avivace4@gmail.com>
2021-09-20 13:33:54 +0200

    Set up Sentry for error reporting

--- oais_platform/settings.py
+++ oais_platform/settings.py
@@ -169,3 +169,24 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Import local settings
 from oais_platform.local_settings import *
+
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+# Sentry
+sentry_sdk.init(
+    dsn=environ.get("SENTRY_DSN"),
+    integrations=[DjangoIntegration()],
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=1.0,
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True,
+    # By default the SDK will try to use the SENTRY_RELEASE
+    # environment variable, or infer a git commit
+    # SHA as release, however you may want to set
+    # something more human-readable.
+    # release="myapp@1.0.0",
+)
