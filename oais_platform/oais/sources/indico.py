c16c3cec93181a12ac01c004014478b44d90cc4f
kchelaki <konstantinos.chelakis@cern.ch>
2021-11-18 09:31:23 +0100

    modified indico to match the current modifications

--- oais_platform/oais/sources/indico.py
+++ oais_platform/oais/sources/indico.py
@@ -6,15 +6,6 @@ from oais_platform.oais.sources.source import Source
 import configparser, os
 
 
-def get_dict_value(dct, keys):
-    for key in keys:
-        try:
-            dct = dct[key]
-        except KeyError:
-            return None
-    return dct
-
-
 class ConfigFileUnavailable(Exception):
     pass
 
@@ -33,22 +24,29 @@ class Indico(Source):
                 f"Could not read config file for Indico instance: {source}"
             )
 
-        if source == "indico":
-            self.config = self.config_file["indico"]
-
-        if not self.config:
-            raise ValueError("No configuration found")
-
     def get_record_url(self, recid):
+        """
+        Returns the API endpoint of the event with the given ID
+        """
         return f"{self.baseURL}/event/{recid}"
 
     def get_record_by_id(self, recid):
+        """
+        Returns the export API endpoint of the event with the given ID
+        """
         return f"{self.baseURL}/export/event/{recid}.json"
 
     def search(self, query, page=1, size=20):
         """
-        makes a GET request to get the number of all the records
+        Look for a record on Indico using the /export/event/ API endpoint
+        given a query.
+        Returns a list of results and a tentatively total numer of results
         """
+
+        # Try to get an idea of the total number of results
+        # Pagination is unreliable on Indico API.
+        # See related ticket: https://github.com/indico/indico/issues/5106
+
         try:
             req = requests.get(
                 self.baseURL + "/search/api/search?q=" + query + "&type=event"
@@ -56,8 +54,9 @@ class Indico(Source):
         except:
             raise ServiceUnavailable("Cannot perform search")
         data = json.loads(req.text)
-        # Get the total number of results for that query
         total_num_hits = int(data["total"])
+
+        # Perform the search
 
         try:
             req = requests.get(
@@ -80,32 +79,21 @@ class Indico(Source):
 
         # Parse JSON response
         data = json.loads(req.text)
-        records_key_list = self.config["results"].split(",")
-        records = get_dict_value(data, records_key_list)
-
+        # Gets the results from the parsed JSON
+        records = data["results"]
         results = []
+
+        # for each record get the recid, the url, the title and the source
         for record in records:
-            recid_key_list = self.config["recid"].split(",")
-            recid = get_dict_value(record, recid_key_list)
-
-            if not isinstance(recid, str):
-                recid = str(recid)
-            url = self.get_record_url(recid)
-            title_key_list = self.config["title"].split(",")
-
-            results.append(
-                {
-                    "url": url,
-                    "recid": recid,
-                    "title": get_dict_value(record, title_key_list),
-                    "authors": [],
-                    "source": self.source,
-                }
-            )
+            results.append(self.parse_record(record))
 
         return {"total_num_hits": total_num_hits, "results": results}
 
     def search_by_id(self, recid):
+        """
+        Look for a record on Indico given a record ID.
+        Returns the resulting record if exists
+        """
         result = []
 
         try:
@@ -121,18 +109,19 @@ class Indico(Source):
         return {"result": result}
 
     def parse_record(self, record):
-        recid_key_list = self.config["recid"].split(",")
-        recid = get_dict_value(record, recid_key_list)
+        """
+        Parses each record returned from the API and returns the necessairy values
+        """
+        recid = record["id"]
         if not isinstance(recid, str):
             recid = str(recid)
 
         url = self.get_record_url(recid)
-        title_key_list = self.config["title"].split(",")
 
         return {
             "url": url,
             "recid": recid,
-            "title": get_dict_value(record, title_key_list),
+            "title": record["title"],
             "authors": [],
             "source": self.source,
         }
