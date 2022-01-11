4a813c3f9b855168320f5ec8f504e228b02dce45
Antonio Vivace <avivace4@gmail.com>
2022-01-11 23:12:12 +0100

    add basic pipeline resolver

---
+++ oais_platform/oais/pipeline.py
@@ -0,0 +1,14 @@
+"""
+Example pipeline definition and resolver
+"""
+
+
+def get_next_steps(taskname):
+    if taskname == 2:
+        return [3]
+    elif taskname == 3:
+        return [4]
+    elif taskname == 4:
+        return [5]
+    elif taskname == 5:
+        return []
