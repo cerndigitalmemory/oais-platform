6b217eab69f8ad6d3a68f778364326761d7230b0
Antonio Vivace <antonio.vivace@cern.ch>
2022-03-16 22:23:00 +0100

    model: extend User model with `Profile` to save additional values

--- oais_platform/oais/models.py
+++ oais_platform/oais/models.py
@@ -1,8 +1,26 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 from . import pipeline
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    indico_api_key = models.TextField(max_length=500, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
 
 
 class Steps(models.IntegerChoices):
