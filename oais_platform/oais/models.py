ca68812e2dbbf840642583f3af3b93d3e7edefde
kchelaki <konstantinos.chelakis@cern.ch>
2022-03-28 14:55:40 +0200

    Add receiver at models

--- oais_platform/oais/models.py
+++ oais_platform/oais/models.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.utils import timezone
 
 from . import pipeline
