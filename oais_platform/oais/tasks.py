cc662779056777addbc1dc95b79e30a235dbd070
amajkic <aleksa.majkic@cern.ch>
2023-08-16 19:59:07 +0200

    Update a variable name Change from `name` to `file_name`

--- oais_platform/oais/tasks.py
+++ oais_platform/oais/tasks.py
@@ -1052,14 +1052,14 @@ def download_files(self, archive_id, step_id):
     folder_name = os.path.join(LOCAL_BASE_PATH, step.output_data)
     os.makedirs(folder_name, exist_ok=True)
 
-    for name, url in data.items():
+    for file_name, url in data.items():
         if number_of_downloaded_files == FILE_LIMIT:
             # TODO: Check what to do if user sends number of files that exceeds the FILE_LIMIT
             return False
 
         response = requests.get(url)
         
-        with open(os.path.join(folder_name, name), "wb") as file:
+        with open(os.path.join(folder_name, file_name), "wb") as file:
             file.write(response.content)
             number_of_downloaded_files += 1
 
