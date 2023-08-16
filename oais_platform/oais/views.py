cd01f66b9ae875d67cb5e7bd8a6246bb4c97efe5
amajkic <aleksa.majkic@cern.ch>
2023-08-16 07:54:18 +0200

    Create an Archive for files from CERNBox

--- oais_platform/oais/views.py
+++ oais_platform/oais/views.py
@@ -517,6 +517,22 @@ class ArchiveViewSet(viewsets.ReadOnlyModelViewSet, PaginationMixin):
             reverse("archives-sgl-details", request=request, kwargs={"pk": archive.id})
         )
 
+    def arhive_created_for_cernbox(self, request):
+        """
+        Creates an Archive for files passed through the CERNBox.
+        """
+        Arhive.objects.create(
+            # recid is an unique value, created as a timestamp
+            # TODO: could timestamps be duplicated?
+            recid=int(time.time()),
+            source="CERNBox",
+            title="Upload from your personal CERNBox",
+            creator=request.user,
+        )
+
+        return Respone({"status" : 0, "errormsg": None})
+
+
     @action(detail=True, methods=["POST"], url_path="delete", url_name="delete")
     def archive_delete(self, request, pk=None):
         """
