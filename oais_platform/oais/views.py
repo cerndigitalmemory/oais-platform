364452eaee49c0ab1da2540e638313c0ce2c7d34
amajkic <aleksa.majkic@cern.ch>
2023-08-21 15:35:19 +0200

    Remove bugs Fix json import Add back the `archive` variable Change `input_data` from request.body to data dictionary

--- oais_platform/oais/views.py
+++ oais_platform/oais/views.py
@@ -998,14 +998,15 @@ def statistics(request):
 @api_view(["POST"])
 # TODO: Add permissions check
 def upload_cernbox(request):
+    data = json.loads(json.dumps(request.data))
     # Checks if the number of files exceeded the allowed maximum number of files permitted to be downloaded
-    if len(json.loads(request.body).keys()) > FILE_LIMIT:
+    if len(data.keys()) > FILE_LIMIT:
         raise BadRequest({"status": 1, "msg": "Too many files send for download"})
 
     timestamp = time.time()
 
     # Creates a new Archive
-    Arhive.objects.create(
+    archive = Archive.objects.create(
         recid=int(timestamp),
         source="CERNBox",
         title="Upload from your personal CERNBox",
@@ -1017,7 +1018,7 @@ def upload_cernbox(request):
         archive=archive,
         name=Steps.DOWNLOAD_FILES_FROM_LINKS,
         status=Status.NOT_RUN,
-        input_data=request.body,
+        input_data=data,
     )
     
     process_files.delay(step.archive.id, step.id)
