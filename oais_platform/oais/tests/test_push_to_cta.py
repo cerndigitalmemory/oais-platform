555bec584c0842ee2978c1337f2c6902e342e24b
Diana Rand <diana.rand@cern.ch>
2025-06-20 10:07:44 +0200

    add test for a successful retry of push_to_cta after waiting for current transfers to finish

--- oais_platform/oais/tests/test_push_to_cta.py
+++ oais_platform/oais/tests/test_push_to_cta.py
@@ -3,12 +3,16 @@ from unittest.mock import MagicMock
 
 from django.apps import apps
 from django.utils import timezone
-from django_celery_beat.models import PeriodicTask
+from django_celery_beat.models import IntervalSchedule, PeriodicTask
 from rest_framework.test import APITestCase
 
 from oais_platform.oais.models import Archive, Status, Step, Steps
 from oais_platform.oais.tasks import push_to_cta
-from oais_platform.settings import FTS_BACKOFF_IN_WEEKS, FTS_CONCURRENCY_LIMIT
+from oais_platform.settings import (
+    FTS_BACKOFF_IN_WEEKS,
+    FTS_CONCURRENCY_LIMIT,
+    FTS_WAIT_IN_HOURS,
+)
 
 
 class PushToCTATests(APITestCase):
@@ -19,6 +23,14 @@ class PushToCTATests(APITestCase):
 
         self.archive = Archive.objects.create(path_to_aip="test/path")
         self.step = Step.objects.create(archive=self.archive, name=Steps.PUSH_TO_CTA)
+        schedule, _ = IntervalSchedule.objects.get_or_create(
+            every=FTS_WAIT_IN_HOURS, period=IntervalSchedule.HOURS
+        )
+        self.periodic_task = PeriodicTask.objects.create(
+            interval=schedule,
+            name=f"Push to CTA: {self.step.id}",
+            task="push_to_cta",
+        )
 
     def test_push_to_cta_success(self):
         self.fts.number_of_transfers.return_value = 0
@@ -53,6 +65,22 @@ class PushToCTATests(APITestCase):
         self.assertEqual(self.fts.push_to_cta.call_count, 0)
         self.assertTrue(
             PeriodicTask.objects.filter(name=f"Push to CTA: {self.step.id}")
+        )
+
+    def test_push_to_cta_retry_after_wait(self):
+        self.fts.number_of_transfers.return_value = 0
+        self.fts.push_to_cta.return_value = "test_job_id"
+        push_to_cta.apply(args=[self.archive.id, self.step.id])
+        self.step.refresh_from_db()
+        self.assertEqual(self.fts.push_to_cta.call_count, 1)
+        self.assertEqual(self.step.status, Status.IN_PROGRESS)
+        self.assertFalse(
+            PeriodicTask.objects.filter(name=f"Push to CTA: {self.step.id}").exists()
+        )
+        self.assertTrue(
+            PeriodicTask.objects.filter(
+                name=f"FTS job status for step: {self.step.id}"
+            ).exists()
         )
 
     def test_push_to_cta_backoff(self):
