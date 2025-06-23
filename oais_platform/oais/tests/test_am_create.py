4a4aeeb692ca4e45a8062d931f98f06381327e7b
Panna Liptak <liptakpanna@gmail.com>
2025-06-23 16:16:06 +0200

    test: add archivematica retry cases

--- oais_platform/oais/tests/test_am_create.py
+++ oais_platform/oais/tests/test_am_create.py
@@ -2,11 +2,13 @@ import json
 from unittest.mock import patch
 
 import requests
+from celery.exceptions import Retry
 from django_celery_beat.models import PeriodicTask
 from rest_framework.test import APITestCase
 
 from oais_platform.oais.models import Archive, Status, Step
 from oais_platform.oais.tasks import archivematica
+from oais_platform.settings import AM_CONCURRENCY_LIMT
 
 
 class ArchivematicaCreateTests(APITestCase):
@@ -111,3 +113,33 @@ class ArchivematicaCreateTests(APITestCase):
         self.assertIn(exception_msg, step_output["errormsg"])
         self.assertEqual(result["status"], 1)
         self.assertIn(exception_msg, result["errormsg"])
+
+    def test_archivematica_retry(self):
+        with patch(
+            "django_celery_beat.models.PeriodicTask.objects.filter"
+        ) as mock_filter:
+            mock_filter.return_value.count.return_value = AM_CONCURRENCY_LIMT
+
+            with self.assertRaises(Retry):
+                archivematica.apply(args=[self.archive.id, self.step.id], throw=True)
+
+            self.step.refresh_from_db()
+            step_output = json.loads(self.step.output_data)
+            msg = "Archivematics is busy, retrying"
+            self.assertEqual(self.step.status, Status.NOT_RUN)
+            self.assertIn(msg, step_output["message"])
+
+    @patch("celery.app.task.Task.request")
+    def test_archivematica_retries_exceeded(self, mock_task_request):
+        with patch(
+            "django_celery_beat.models.PeriodicTask.objects.filter"
+        ) as mock_filter:
+            mock_filter.return_value.count.return_value = AM_CONCURRENCY_LIMT
+            mock_task_request.retries = 10
+            archivematica.apply(args=[self.archive.id, self.step.id], throw=True)
+
+            self.step.refresh_from_db()
+            step_output = json.loads(self.step.output_data)
+            msg = "Archivematica max retries exceeded"
+            self.assertEqual(self.step.status, Status.FAILED)
+            self.assertIn(msg, step_output["errormsg"])
