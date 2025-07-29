c77670ca637ac590a43bade85488c90d7c51f9da
Diana Rand <diana.rand@cern.ch>
2025-07-29 14:55:10 +0200

    improve tests and add additional ones for new statistics endpoint

--- oais_platform/oais/tests/test_step_statistics.py
+++ oais_platform/oais/tests/test_step_statistics.py
@@ -6,38 +6,37 @@ from oais_platform.oais.models import Archive, Status, Step, Steps
 
 
 class StepStatisticsEndpointTest(APITestCase):
+    def create_archive_with_steps(self, steps_to_complete):
+        archive = Archive.objects.create()
+        for step_name in steps_to_complete:
+            Step.objects.create(
+                name=step_name, status=Status.COMPLETED, archive=archive
+            )
+        archive.save()
+        return archive
+
     def setUp(self):
         self.url = reverse("step_statistics")
-
-        harvested_archive = Archive.objects.create()
-        preserved_archive = Archive.objects.create()
-        tape_archive = Archive.objects.create()
-        registry_archive = Archive.objects.create()
-        tape_and_registry_archive = Archive.objects.create()
-        step_data = {
-            harvested_archive: [Steps.CHECKSUM],
-            preserved_archive: [Steps.CHECKSUM, Steps.ARCHIVE],
-            tape_archive: [
-                Steps.CHECKSUM,
-                Steps.ARCHIVE,
-                Steps.PUSH_TO_CTA,
-            ],
-            registry_archive: [
+        self.create_archive_with_steps([Steps.CHECKSUM])
+        self.create_archive_with_steps([Steps.CHECKSUM, Steps.ARCHIVE])
+        self.create_archive_with_steps(
+            [Steps.CHECKSUM, Steps.ARCHIVE, Steps.PUSH_TO_CTA]
+        )
+        self.create_archive_with_steps(
+            [
                 Steps.CHECKSUM,
                 Steps.ARCHIVE,
                 Steps.INVENIO_RDM_PUSH,
-            ],
-            tape_and_registry_archive: [
+            ]
+        )
+        self.create_archive_with_steps(
+            [
                 Steps.CHECKSUM,
                 Steps.ARCHIVE,
                 Steps.PUSH_TO_CTA,
                 Steps.INVENIO_RDM_PUSH,
-            ],
-        }
-        for archive, steps in step_data.items():
-            for step in steps:
-                Step.objects.create(name=step, status=Status.COMPLETED, archive=archive)
-            archive.save()
+            ]
+        )
 
     def test_step_statistics(self):
         response = self.client.get(self.url, format="json")
@@ -54,31 +53,20 @@ class StepStatisticsEndpointTest(APITestCase):
         )
 
     def test_step_statistics_more_archives(self):
-        tape_archive = Archive.objects.create()
-        registry_archive = Archive.objects.create()
-        tape_and_registry_archive = Archive.objects.create()
-        step_data = {
-            tape_archive: [
+        self.create_archive_with_steps(
+            [
                 Steps.CHECKSUM,
                 Steps.ARCHIVE,
                 Steps.PUSH_TO_CTA,
-            ],
-            registry_archive: [
+            ]
+        )
+        self.create_archive_with_steps(
+            [
                 Steps.CHECKSUM,
                 Steps.ARCHIVE,
                 Steps.INVENIO_RDM_PUSH,
-            ],
-            tape_and_registry_archive: [
-                Steps.CHECKSUM,
-                Steps.ARCHIVE,
-                Steps.PUSH_TO_CTA,
-                Steps.INVENIO_RDM_PUSH,
-            ],
-        }
-        for archive, steps in step_data.items():
-            for step in steps:
-                Step.objects.create(name=step, status=Status.COMPLETED, archive=archive)
-            archive.save()
+            ]
+        )
 
         response = self.client.get(self.url, format="json")
         self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -89,6 +77,52 @@ class StepStatisticsEndpointTest(APITestCase):
                 "harvested_preserved_count": 1,
                 "harvested_preserved_tape_count": 2,
                 "harvested_preserved_registry_count": 2,
-                "harvested_preserved_tape_registry_count": 2,
+                "harvested_preserved_tape_registry_count": 1,
+            },
+        )
+
+    def test_step_statistics_empty_database(self):
+        Archive.objects.all().delete()
+        Step.objects.all().delete()
+
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "only_harvested_count": 0,
+                "harvested_preserved_count": 0,
+                "harvested_preserved_tape_count": 0,
+                "harvested_preserved_registry_count": 0,
+                "harvested_preserved_tape_registry_count": 0,
+            },
+        )
+
+    def test_step_statistics_mixed_status_steps(self):
+        archive_mixed_status = self.create_archive_with_steps(
+            [Steps.CHECKSUM, Steps.ARCHIVE]
+        )
+        Step.objects.create(
+            name=Steps.PUSH_TO_CTA,
+            status=Status.IN_PROGRESS,
+            archive=archive_mixed_status,
+        )
+        Step.objects.create(
+            name=Steps.INVENIO_RDM_PUSH,
+            status=Status.FAILED,
+            archive=archive_mixed_status,
+        )
+        archive_mixed_status.save()
+
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(
+            response.data,
+            {
+                "only_harvested_count": 1,
+                "harvested_preserved_count": 2,
+                "harvested_preserved_tape_count": 1,
+                "harvested_preserved_registry_count": 1,
+                "harvested_preserved_tape_registry_count": 1,
             },
         )
