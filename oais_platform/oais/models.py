66b1f22b933b23c64b960b46ace36020628c50d0
Panna Liptak <liptakpanna@gmail.com>
2025-08-06 13:30:50 +0200

    add set sip size

--- oais_platform/oais/models.py
+++ oais_platform/oais/models.py
@@ -168,6 +168,10 @@ class Archive(models.Model):
         self.title = title
         self.save()
 
+    def set_sip_size(self, size):
+        self.sip_size = size
+        self.save()
+
     def update_sip_size(self):
         self.sip_size = sum(
             file.stat().st_size for file in Path(self.path_to_sip).rglob("*")
