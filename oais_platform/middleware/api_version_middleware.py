b3130255ef4b1303d4c375999f7fbf2bc1ecd5e3
Wesley Middelbos <wesley.middelbos@cern.ch>
2025-10-06 08:36:51 +0200

    Update middlware class to correct form

--- oais_platform/middleware/api_version_middleware.py
+++ oais_platform/middleware/api_version_middleware.py
@@ -3,7 +3,10 @@ from oais_platform import settings
 
 class api_version_middleware(get_response):
 
-    def middleware(request):
-        response = get_response(request)
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        response = self.get_response(request)
         response["X-API-Version"] = settings.APP_VERSION
         return response
