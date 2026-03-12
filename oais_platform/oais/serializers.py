3a1425a3ce10359cf1f73f2d5da3e092f16e4213
Panna Liptak <panna.georgina.liptak@cern.ch>
2026-03-12 15:46:29 +0100

    Merging jobs and tags into Collections

--- oais_platform/oais/serializers.py
+++ oais_platform/oais/serializers.py
@@ -240,6 +240,7 @@ class CollectionSerializer(serializers.ModelSerializer):
             "creator",
             "timestamp",
             "last_modification_date",
+            "internal",
             "archives_count",
             "archives_summary",
             "archives_sip_count",
