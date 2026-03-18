908443619ca50e1eb172814b2364abdb8e1a118a
Panna Liptak <panna.georgina.liptak@cern.ch>
2026-03-18 15:24:07 +0100

    Updated

--- oais_platform/oais/management/commands/cleanup_db.py
+++ oais_platform/oais/management/commands/cleanup_db.py
@@ -1,3 +1,4 @@
+import json
 import os
 import shutil
 
@@ -5,7 +6,15 @@ from django.core.management.base import BaseCommand
 from django.db import transaction
 from django_celery_beat.models import PeriodicTask
 
-from oais_platform.oais.models import Archive, Collection, HarvestRun, Resource
+from oais_platform.oais.models import (
+    COMPLETED_STATUSES,
+    Archive,
+    Collection,
+    HarvestRun,
+    Resource,
+    Step,
+    StepType,
+)
 from oais_platform.settings import ENVIRONMENT
 
 
@@ -44,28 +53,44 @@ class Command(BaseCommand):
             return
 
         self.stdout.write(self.style.SUCCESS("Starting clean up script..."))
-        archives = Archive.objects.only("id", "path_to_sip", "path_to_aip")
-
-        ids_to_delete = []
 
         self.stdout.write(self.style.SUCCESS("Deleting SIPs and AIPs..."))
-        for a in archives.iterator():
-            self.stdout.write(f"Archive {a.id}")
-            if self.safe_delete(a.path_to_sip):
-                self.stdout.write(self.style.SUCCESS("Deleted SIP"))
-            if self.safe_delete(a.path_to_aip):
-                self.stdout.write(self.style.SUCCESS("Deleted AIP"))
-            ids_to_delete.append(a.id)
+        step_types = StepType.objects.filter(has_sip=True) | StepType.objects.filter(
+            has_aip=True
+        )
+
+        outputs = Step.objects.filter(
+            step_type__in=step_types,
+            status__in=COMPLETED_STATUSES,
+            output_data__isnull=False,
+        ).values_list("output_data", "archive_id")
+
+        for output, archive_id in outputs:
+            try:
+                artifact = json.loads(output).get("artifact")
+                if artifact:
+                    artifact_path = artifact.get("artifact_path")
+                    if artifact_path and self.safe_delete(artifact_path):
+                        self.stdout.write(
+                            self.style.SUCCESS(
+                                f"Deleted {artifact.get('artifact_name')} from archive {archive_id}"
+                            )
+                        )
+            except json.JSONDecodeError:
+                self.stdout.write(
+                    self.style.ERROR(f"Invalid JSON in output for archive {archive_id}")
+                )
 
         self.stdout.write(self.style.SUCCESS("Deleting database objects..."))
         with transaction.atomic():
-            Archive.objects.filter(id__in=ids_to_delete).delete()
+            Archive.objects.all().delete()
+            Step.objects.all().delete()
             HarvestRun.objects.all().delete()
             Collection.objects.all().delete()
             Resource.objects.all().delete()
-            PeriodicTask.objects.filter(task="check_am_status").all().delete()
+            PeriodicTask.objects.filter(task="check_am_status").delete()
 
-        self.stdout.write(self.style.SUCCESS("Clean up completed successfully."))
+        self.stdout.write(self.style.SUCCESS("Clean up completed."))
         self.stdout.write(
             self.style.SUCCESS(
                 "Make sure the clean up the file system in case there are any remaining files."
