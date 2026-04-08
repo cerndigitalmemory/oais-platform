a14518c5389fc5e22807dd3abb30200470e6dff8
Diana Rand <diana.rand@cern.ch>
2026-04-08 13:01:25 +0200

    add tests

--- oais_platform/oais/tests/test_cta_manager.py
+++ oais_platform/oais/tests/test_cta_manager.py
@@ -130,3 +130,59 @@ class CTAManagerTests(APITestCase):
         self.step.refresh_from_db()
         self.assertEqual(self.step.status, Status.FAILED)
         self.assertEqual(self.step.failure_type, StepFailureType.MISSING_INPUT_DATA)
+
+    @patch("oais_platform.oais.tasks.cta.push_to_cta.delay")
+    def test_cta_manager_triggers_transfer_after_finished_job(self, mock_push_to_cta):
+        self.step.step_type.concurrency_limit = 1
+        self.step.step_type.save()
+
+        self.step.status = Status.IN_PROGRESS
+        self.step.set_output_data({"fts_job_id": "job_id"})
+        self.step.save()
+
+        waiting_archive = Archive.objects.create(
+            path_to_aip="basepath/aips/test/path/waiting.zip"
+        )
+        waiting_step = Step.objects.create(
+            archive=waiting_archive,
+            step_name=StepName.PUSH_TO_CTA,
+            status=Status.WAITING,
+            start_date=timezone.now(),
+        )
+        waiting_archive.set_last_step(waiting_step)
+        self.fts.job_statuses.return_value = [
+            {"job_id": "job_id", "job_state": "FINISHED"}
+        ]
+
+        cta_manager.apply()
+        mock_push_to_cta.assert_called_once_with(waiting_archive.id, waiting_step.id)
+
+    @patch("oais_platform.oais.tasks.cta.create_retry_step.apply_async")
+    @patch("oais_platform.oais.tasks.cta.push_to_cta.delay")
+    def test_cta_manager_triggers_transfer_after_failed_job(
+        self, mock_push_to_cta, mock_retry
+    ):
+        self.step.step_type.concurrency_limit = 1
+        self.step.step_type.save()
+
+        self.step.status = Status.IN_PROGRESS
+        self.step.set_output_data({"fts_job_id": "job_id"})
+        self.step.save()
+
+        waiting_archive = Archive.objects.create(
+            path_to_aip="basepath/aips/test/path/waiting.zip"
+        )
+        waiting_step = Step.objects.create(
+            archive=waiting_archive,
+            step_name=StepName.PUSH_TO_CTA,
+            status=Status.WAITING,
+            start_date=timezone.now(),
+        )
+        waiting_archive.set_last_step(waiting_step)
+
+        self.fts.job_statuses.return_value = [
+            {"job_id": "job_id", "job_state": "FAILED"}
+        ]
+
+        cta_manager.apply()
+        mock_push_to_cta.assert_called_once_with(waiting_archive.id, waiting_step.id)
