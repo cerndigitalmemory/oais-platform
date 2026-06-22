4016d75291c265e84a2e3e816045a2460954c7ce
Wesley Middelbos <wesley.middelbos@cern.ch>
2026-06-22 15:29:30 +0200

    Distribute Archivematica transfers by instance capacity

--- oais_platform/oais/archivematica_instances.py
+++ oais_platform/oais/archivematica_instances.py
@@ -1,18 +1,10 @@
 import random
 
-from oais_platform.oais.models import Archive, Step
+from oais_platform.oais.models import Step
 from oais_platform.settings import AM_INSTANCES
 
 
 class ArchivematicaInstances:
-
-    @staticmethod
-    def assign(step: Step):
-        am_instance_config = random.choice(AM_INSTANCES)
-        step.set_input_data_field(
-            "archivematica_instance", am_instance_config["AM_INSTANCE"]
-        )
-        return am_instance_config
 
     @staticmethod
     def get_instance_config(archivematica_instance):
