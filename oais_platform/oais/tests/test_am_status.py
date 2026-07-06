5fe9ff4480e4fc11b15ca5080e778a1bd763fa45
Wesley Middelbos <wesley.middelbos@cern.ch>
2026-07-06 16:53:24 +0200

    Move terminal statuses to enums

--- oais_platform/oais/tests/test_am_status.py
+++ oais_platform/oais/tests/test_am_status.py
@@ -97,9 +97,6 @@ class ArchivematicaStatusTests(APITestCase):
 
     @patch("amclient.AMClient.get_unit_status")
     def test_am_status_completed_not_fully(self, get_unit_status):
-        self.step.finish_date = timezone.now()
-        self.step.save()
-
         get_unit_status.return_value = {
             "status": "COMPLETE",
             "microservice": "Completed first half, still processing",
@@ -118,7 +115,6 @@ class ArchivematicaStatusTests(APITestCase):
             get_unit_status.return_value["microservice"],
         )
         self.assertRaises(KeyError, lambda: self.step.output_data_json["artifact"])
-        self.assertIsNone(self.step.finish_date)
         self.assertIsNone(self.step.output_data_json.get("retry_count", None))
         self.assertIsNone(self.step.output_data_json.get("retry", None))
         self.assertIsNone(self.step.output_data_json.get("errormsg", None))
@@ -183,9 +179,6 @@ class ArchivematicaStatusTests(APITestCase):
 
     @patch("amclient.AMClient.get_unit_status")
     def test_am_status_processing(self, get_unit_status):
-        self.step.finish_date = timezone.now()
-        self.step.save()
-
         get_unit_status.return_value = {
             "status": "PROCESSING",
             "microservice": "Package is being processed",
@@ -203,7 +196,6 @@ class ArchivematicaStatusTests(APITestCase):
             get_unit_status.return_value["microservice"],
         )
         self.assertRaises(KeyError, lambda: self.step.output_data_json["artifact"])
-        self.assertIsNone(self.step.finish_date)
         self.assertIsNone(self.step.output_data_json.get("retry_count", None))
         self.assertIsNone(self.step.output_data_json.get("retry", None))
         self.assertIsNone(self.step.output_data_json.get("errormsg", None))
@@ -233,7 +225,6 @@ class ArchivematicaStatusTests(APITestCase):
         get_jobs.return_value = 1
 
         self.step.status = Status.SUBMITTED
-        self.step.finish_date = timezone.now()
         self.step.save()
 
         check_am_status.apply(args=[self.step.id])
@@ -247,7 +238,6 @@ class ArchivematicaStatusTests(APITestCase):
             "Waiting for archivematica to respond",
         )
         self.assertRaises(KeyError, lambda: self.step.output_data_json["artifact"])
-        self.assertIsNone(self.step.finish_date)
 
     @patch("oais_platform.oais.tasks.archivematica.create_retry_step.apply_async")
     @patch("amclient.AMClient.get_jobs")
