91d824a19366e618879c2b0335c0e20fd6c497c9
Diana Rand <diana.rand@cern.ch>
2026-07-10 11:20:47 +0000

    include failures with no failure type in "other" count

--- oais_platform/oais/tests/test_step_failure_statistics.py
+++ oais_platform/oais/tests/test_step_failure_statistics.py
@@ -104,6 +104,26 @@ class StepFailureStatisticsEndpointTest(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, [])
 
+    def test_failed_step_without_failure_type(self):
+        self.create_archive_with_steps(
+            [
+                (StepName.ARCHIVE, Status.FAILED, StepFailureType.OTHER),
+            ]
+        )
+        self.create_archive_with_steps(
+            [
+                (StepName.ARCHIVE, Status.FAILED, None),
+            ]
+        )
+
+        response = self.client.get(self.url, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertFalse(any(row["failure_type"] is None for row in response.data))
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(
+            self.get_count(response.data, StepName.ARCHIVE, StepFailureType.OTHER), 2
+        )
+
     def test_retried_step_counted_when_latest_attempt_fails(self):
         self.create_archive_with_steps(
             [
