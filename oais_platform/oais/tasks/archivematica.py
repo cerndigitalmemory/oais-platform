4c0e7519211b2dc5841c92d501bda9445ff373ae
Wesley Middelbos <wesley.middelbos@cern.ch>
2026-07-10 21:06:31 +0200

    Splitted up archivamtia, check_am_status functions

--- oais_platform/oais/tasks/archivematica.py
+++ oais_platform/oais/tasks/archivematica.py
@@ -506,7 +506,7 @@ def get_am_client(step):
         am.ss_url = am_instance_config["AM_SS_URL"]
         am.ss_user_name = am_instance_config["AM_SS_USERNAME"]
         am.ss_api_key = am_instance_config["AM_SS_API_KEY"]
-        am.processing_config = "automated"
+        am.processing_configneeds = "automated"
         if not am_instance_config.get("AM_TRANSFER_SOURCE"):
             am_instance_config["AM_TRANSFER_SOURCE"] = get_transfer_source(am)
         am.transfer_source = am_instance_config["AM_TRANSFER_SOURCE"]
